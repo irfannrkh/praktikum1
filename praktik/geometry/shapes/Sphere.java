f48ddbfefd8b8c85141b7926b4d8987918ebc9b8
irfannrkh <hakim030606@gmail.com>
2025-04-25 02:26:30 +0700

    Update Sphere.java

--- praktik/geometry/shapes/Sphere.java
+++ praktik/geometry/shapes/Sphere.java
@@ -10,38 +10,44 @@ import praktik.geometry.interfaces.Weightable;
  *
  * @author Lenovo
  */
-
-
 public class Sphere extends CircularShape implements ThreeDimensional, Weightable {
-    private double mass;
+    private double mass;  // atribut massa untuk menghitung berat
 
-    public Sphere() {}
+    public Sphere() {} // Konstruktor(tanpa parameter)
 
+    // Konstruktor dengan parameter radius dan mass
     public Sphere(double radius, double mass) {
-        this.radius = radius;
-        this.mass = mass;
+        this.radius = radius; // Mengisi atribut radius dari superclass CircularShape
+        this.mass = mass; // Mengisi atribut massa
     }
 
     @Override
+     // Override method getSurfaceArea dari interface ThreeDimensional
     public double getSurfaceArea() {
+         // Rumus luas permukaan bola = 4πr^2
         return 4 * getPi() * radius * radius;
     }
 
     @Override
+     // Override method getVolume dari interface ThreeDimensional
     public double getVolume() {
-        return (4.0 / 3.0) * getPi() * radius * radius * radius;
+        return (4.0 / 3.0) * getPi() * radius * radius * radius; // Rumus volume bola = 4/3 π r^3
+
     }
 
     @Override
+    // Override method getWeight dari interface Weightable
     public double getWeight() {
+       // Berat = massa * gravitasi
         return mass * GRAVITY;
     }
 
+     // Override method printInfo dari superclass atau interface
     @Override
     public void printInfo() {
-        System.out.println("Name         : Sphere");
-        System.out.printf("Surface area : %.2f\n", getSurfaceArea());
-        System.out.printf("Volume       : %.2f\n", getVolume());
-        System.out.printf("Weight       : %.2f\n", getWeight());
+        System.out.println("Name         : Sphere"); // print nama bentuk
+        System.out.printf("Surface area : %.2f\n", getSurfaceArea()); // print luas permukaan
+        System.out.printf("Volume       : %.2f\n", getVolume()); // print volume
+        System.out.printf("Weight       : %.2f\n", getWeight()); // print berat
     }
 }
