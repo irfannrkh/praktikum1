03b3823bb5c8c6d6f9f2b11413727cdb8c7a51e2
irfannrkh <hakim030606@gmail.com>
2025-04-25 02:47:25 +0700

    Update CircularShape.java

--- praktik/geometry/bases/CircularShape.java
+++ praktik/geometry/bases/CircularShape.java
@@ -8,22 +8,27 @@ package praktik.geometry.bases;
  *
  * @author Lenovo
  */
-// Kelas untuk lingkaran
+ // class abstrak CircularShape turunan dari Shape
 public abstract class CircularShape extends Shape {
-    protected double radius; 
-    protected final int PI_NUMERATOR = 22; // final itu tetap
-    protected final int PI_DENOMINATOR = 7; // final itu tetap
+    protected double radius;  // Atribut radius bisa diakses oleh subclass
+    protected final int PI_NUMERATOR = 22; // final itu tetap nilainya
+    protected final int PI_DENOMINATOR = 7; // final itu tetap nilainya
 
+    // Konstruktor default tanpa parameter
     public CircularShape() {}
 
+    // Method setter untuk mengatur nilai radius
     public void setRadius(double inputRadius) {
         this.radius = inputRadius;
     }
 
+    // Method getter untuk mengambil nilai radius
     public double getRadius() {
         return radius;
     }
 
+     // Method protected untuk menghitung dan mengembalikan nilai π (phi)
+    // gunakan pembagian PI_NUMERATOR / PI_DENOMINATOR, yaitu 22/7
     protected double getPi() {
         return (double) PI_NUMERATOR / PI_DENOMINATOR;
     }
