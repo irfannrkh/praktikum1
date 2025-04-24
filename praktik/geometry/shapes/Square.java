5823c2c448c7452dca95f1a962f7b8cbd25617e2
irfannrkh <hakim030606@gmail.com>
2025-04-25 02:31:43 +0700

    Update Square.java

--- praktik/geometry/shapes/Square.java
+++ praktik/geometry/shapes/Square.java
@@ -10,30 +10,35 @@ import praktik.geometry.interfaces.TwoDimensional;
  * @author Lenovo
  */
 
-
+// class Square sebagai subclass dari Shape dan mengimplementasikan interface TwoDimensional
 public class Square extends Shape implements TwoDimensional {
     private double side;
-
+    
+// Konstruktor(tanpa parameter)
     public Square() {}
 
+    // Konstruktor dengan parameter panjang sisi
     public Square(double side) {
-        this.side = side;
+        this.side = side; // Menginisialisasi atribut side
     }
 
+     // Override method getArea dari interface TwoDimensional
     @Override
     public double getArea() {
+        // Rumus luas persegi = sisi * sisi
         return side * side;
     }
 
+    // Override method getPerimeter dari interface TwoDimensional
     @Override
     public double getPerimeter() {
-        return 4 * side;
+        return 4 * side; // Rumus keliling persegi = 4 * sisi
     }
 
-    @Override
+    @Override  // Override method printInfo untuk mencetak informasi bentuk persegi
     public void printInfo() {
-        System.out.println("Name       : Square");
-        System.out.printf("Area       : %.2f\n", getArea());
-        System.out.printf("Perimeter  : %.2f\n", getPerimeter());
+        System.out.println("Name       : Square"); // print nama bentuk
+        System.out.printf("Area       : %.2f\n", getArea()); // print luas persegi
+        System.out.printf("Perimeter  : %.2f\n", getPerimeter()); // print keliling persegi
     }
 }
